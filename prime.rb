4c3bdacb446ae53466e97e928ea192dd1de925e8
Nate Tan <nktan93@gmail.com>
2019-03-01 17:46:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+
+def range(num)
+  (2...num).to_a
+end
+
+def remainders(num)
+  array = []
+  range(num).each do |x|
+    array << num % x
+  end
+  array
+end
+
+def prime?(num)
+  if num > 1
+    !remainders(num).include?(0)
+  else
+    false
+  end
+end
